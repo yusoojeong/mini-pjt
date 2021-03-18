fa4d87718e85963959dee9750c3d045b26086992
yusoojeong <yusj1231@gmail.com>
2021-03-18 11:49:05 +0900

    [Add] restaurants app
    restaurants is an app for restaurant API functions
    [Update] settings.py - runserver and add app
    
    Signed-off-by: yusoojeong <yusj1231@gmail.com>

--- mini_pjt/mini_pjt/settings.py
+++ mini_pjt/mini_pjt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5-k_6z(lvhgimyoqe6!gc^_2hxh2img4sur@s%9(x70#7y13#0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'restaurants',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
