c6e5a634f15bd2e29e52d00af8d722b739b6700b
yusoojeong <yusj1231@gmail.com>
2021-03-18 14:32:43 +0900

    [Add] def - restaurant create and read
    Signed-off-by: yusoojeong <yusj1231@gmail.com>

--- mini_pjt/restaurants/views.py
+++ mini_pjt/restaurants/views.py
@@ -9,20 +9,31 @@ from .serializers import RestaurantSerializer, MenuSerializer
 
 
 # 음식점 Create
+@api_view(['POST'])
 def create(request):
-    pass
+    serializer = RestaurantSerializer(data=request.data)
+
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+
+        return Response(serializer.data)
 
 
 # 음식점 목록
+@api_view(['GET'])
 def index(request, page):
-    pass
+    restaurant = get_object_or_404(Restaurant, pk=page)
+    serializer = RestaurantSerializer(restaurant)
+    return Response(serializer.data)
 
 
 # 음식점 자세한 내용
+@api_view(['GET'])
 def detail(request, restaurant_id):
     pass
 
 
 # 음식점의 메뉴 Create
+@api_view(['POST'])
 def menu_create(request, restaurant_id):
     pass
