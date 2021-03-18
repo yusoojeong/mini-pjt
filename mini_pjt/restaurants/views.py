bf4e34f568ab8d9004ffcca456ed52517f4d9af4
yusoojeong <yusj1231@gmail.com>
2021-03-18 17:27:43 +0900

    [Update] restaurant app CR function
    [Update] serializers, urls, views
    check postman
    
    Signed-off-by: yusoojeong <yusj1231@gmail.com>

--- mini_pjt/restaurants/views.py
+++ mini_pjt/restaurants/views.py
@@ -41,13 +41,23 @@ def detail(request, page):
     return Response(serializer.data)
 
 
-# 음식점의 메뉴 Create
+# 메뉴 Create
 @api_view(['POST'])
-def menu_create(request, page):
+def menu_create(request):
     serializer = MenuSerializer(data=request.data)
-    restaurant = get_object_or_404(Restaurant, pk=page)
     
     if serializer.is_valid(raise_exception=True):
-        serializer.save(restaurant=restaurant)
+        serializer.save()
 
         return Response(serializer.data)
+
+# 음식점 메뉴 추가
+@api_view(['POST'])
+def menu_add(reqeust, menu_id, rest_id):
+    menu = get_object_or_404(Menu, pk=menu_id)
+    restaurant = get_object_or_404(Restaurant, pk=rest_id)    
+    if menu not in restaurant.menus.all():
+        restaurant.menus.add(menu)
+        return Response(status=200)
+    else:
+        return Response(status=400)
